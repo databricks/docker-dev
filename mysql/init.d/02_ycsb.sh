bd0fc195ff6e1f531936e1d8c332d5271c68501f
robert-s-lee <sangkyulee@gmail.com>
2023-06-09 17:30:41 -0400

    pg and mysql ycsb data

---
+++ mysql/init.d/02_ycsb.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+ycsb_create_mysql() {
+cat <<'EOF'
+CREATE TABLE IF NOT EXISTS THEUSERTABLE (
+    YCSB_KEY INT PRIMARY KEY,
+    FIELD0 TEXT, FIELD1 TEXT,
+    FIELD2 TEXT, FIELD3 TEXT,
+    FIELD4 TEXT, FIELD5 TEXT,
+    FIELD6 TEXT, FIELD7 TEXT,
+    FIELD8 TEXT, FIELD9 TEXT,
+    TS TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    INDEX (TS)
+);
+EOF
+}
+
+ycsb_load() {
+    local ROLE=${1}
+    local DB_ARC_USER=${2} 
+    local DB_ARC_PW=${3} 
+    local DB_DB=${4} 
+    local SIZE_FACTOR=${5}
+    set -x
+
+    rm /tmp/ycsb.fifo.$$ 2>/dev/null
+    mkfifo /tmp/ycsb.fifo.$$
+    seq 0 $(( 1000000*${SIZE_FACTOR:-1} - 1 )) > /tmp/ycsb.fifo.$$ &
+    ycsb_create_mysql | mysql -u ${DB_ARC_USER}${SIZE_FACTOR} --password=${DB_ARC_PW} -D ${DB_DB}${SIZE_FACTOR}
+
+    echo "load data local infile '/tmp/ycsb.fifo.$$' into table THEUSERTABLE (YCSB_KEY);" | \
+        mysql -u ${DB_ARC_USER}${SIZE_FACTOR} \
+            --password=${DB_ARC_PW} \
+            -D ${DB_DB}${SIZE_FACTOR} \
+            --local-infile
+    rm /tmp/ycsb.fifo.$$
+
+    set +x
+}
+# 1M, 10M and 100M rows
+ycsb_load SRC ${SRCDB_ARC_USER} ${SRCDB_ARC_PW} ${SRCDB_DB}  
+ycsb_load SRC ${SRCDB_ARC_USER} ${SRCDB_ARC_PW} ${SRCDB_DB} 10 
+ycsb_load SRC ${SRCDB_ARC_USER} ${SRCDB_ARC_PW} ${SRCDB_DB} 100
--- pg/init.d/01_user.sh
+++ pg/init.d/01_user.sh
@@ -1,17 +1,34 @@
-#!/usr/env/bin bash
+#!/usr/bin/env bash
 
-# src
-psql --username "$POSTGRES_USER" <<EOF
-CREATE USER ${SRCDB_ARC_USER} PASSWORD '${SRCDB_ARC_PW}';
-ALTER USER ${SRCDB_ARC_USER} CREATEDB;
-ALTER ROLE ${SRCDB_ARC_USER} WITH REPLICATION;
-CREATE DATABASE ${SRCDB_DB} WITH OWNER ${SRCDB_ARC_USER} ENCODING 'UTF8';
+create_user() {
+    local ROLE=${1}
+    local DB_ARC_USER=${2} 
+    local DB_ARC_PW=${3} 
+    local DB_DB=${4} 
+    local SIZE_FACTOR=${5}
+
+    psql --username "$POSTGRES_USER" <<EOF
+    CREATE USER ${DB_ARC_USER}${SIZE_FACTOR} PASSWORD '${DB_ARC_PW}';
+    ALTER USER ${DB_ARC_USER}${SIZE_FACTOR} CREATEDB;
+    ALTER ROLE ${DB_ARC_USER}${SIZE_FACTOR} WITH REPLICATION;
+    CREATE DATABASE ${DB_DB}${SIZE_FACTOR} WITH OWNER ${DB_ARC_USER}${SIZE_FACTOR} ENCODING 'UTF8';
 EOF
 
-# dst
-psql --username "$POSTGRES_USER" <<EOF
-CREATE USER ${DSTDB_ARC_USER} PASSWORD '${DSTDB_ARC_PW}';
-ALTER USER ${DSTDB_ARC_USER} CREATEDB;
-ALTER ROLE ${DSTDB_ARC_USER} WITH REPLICATION;
-CREATE DATABASE ${DSTDB_DB} WITH OWNER ${DSTDB_ARC_USER} ENCODING 'UTF8';
+    if [ "${ROLE^^}" = "SRC" ]; then
+
+    psql --username "$POSTGRES_USER" <<EOF
+    SELECT 'init' FROM pg_create_logical_replication_slot('${DB_DB}${SIZE_FACTOR}_w2j', 'wal2json');
+    SELECT * from pg_replication_slots;
 EOF
+
+    fi
+}
+
+create_user "SRC" ${SRCDB_ARC_USER} ${SRCDB_ARC_PW} ${SRCDB_DB}  
+create_user "DST" ${DSTDB_ARC_USER} ${DSTDB_ARC_PW} ${DSTDB_DB}  
+
+create_user "SRC" ${SRCDB_ARC_USER} ${SRCDB_ARC_PW} ${SRCDB_DB} 10 
+create_user "DST" ${DSTDB_ARC_USER} ${DSTDB_ARC_PW} ${DSTDB_DB} 10 
+
+create_user "SRC" ${SRCDB_ARC_USER} ${SRCDB_ARC_PW} ${SRCDB_DB} 100 
+create_user "DST" ${DSTDB_ARC_USER} ${DSTDB_ARC_PW} ${DSTDB_DB} 100 
