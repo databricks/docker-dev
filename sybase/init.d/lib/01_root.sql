df672143206734cfc6b9e4183c192837815c3c94
robert-s-lee <sangkyulee@gmail.com>
2023-06-25 20:57:40 -0400

    sqlserver and sybase

---
+++ sybase/init.d/lib/01_root.sql
@@ -0,0 +1,21 @@
+disk resize name='master', size='100G'
+go
+
+sp_cacheconfig "default data cache", "25M"
+go
+
+master..sp_dboption master 'select into/bulkcopy/pllsort' enable
+go
+
+sp_addlogin ${DB_ARC_USER}, ${DB_ARC_PW}, master 
+go
+
+-- add user into the database for the login
+use master
+go
+
+sp_adduser ${DB_ARC_USER}
+go
+
+grant all to ${DB_ARC_USER}
+go
---
+++ sybase/init.d/lib/02_root.sql
@@ -0,0 +1,18 @@
+create database ${DB_DB} on master = '100G' LOG ON master = '20G' WITH OVERRIDE
+go
+
+sp_addlogin ${DB_ARC_USER}, ${DB_ARC_PW}, ${DB_DB}
+go
+
+master..sp_dboption ${DB_DB}, 'select into/bulkcopy/pllsort', true
+go
+
+-- add user into the database for the login
+use ${DB_DB}
+go
+
+sp_adduser ${DB_ARC_USER}
+go
+
+grant all to ${DB_ARC_USER}
+go
