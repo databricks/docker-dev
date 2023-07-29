dfda9a124a0395231d5c71fa123918c4f0832f98
Robert S Lee <sangkyulee@gmail.com>
2023-07-29 17:37:43 -0400

    fix informix dst tbx size for ycsb

--- bin/recdemo.sh
+++ bin/recdemo.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-export RECDEMO_DIR=$(pwd)/$(dirname ${BASH_SOURCE[0]})
+export RECDEMO_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 . ${RECDEMO_DIR}/startdb.sh
 
+
 rec_pipeline() {
-    RECFILENAME=${REPL_TYPE}_${SOURCE}_${DESTINATION}.ascii.cast
-    RECFILE=${REC_DIR}/${RECFILENAME}
-    echo ${REPL_TYPE} ${SOURCE} ${DESTINATION} $RECFILE
+    export REPL_TYPE=$1
+    export SOURCE=$2
+    export TARGET=$3
+
+    RECFILENAME=${REPL_TYPE}_${SOURCE}_${TARGET}.ascii.cast
+    export RECFILE=${REC_DIR}/${RECFILENAME}
+
+    echo ${REPL_TYPE} ${SOURCE} ${TARGET} $RECFILE
+
     STARTTIME=$(date +%s)
-    ./recdemo.expect
+    ${RECDEMO_DIR}/recdemo.expect
     ENDTIME=$(date +%s)
     # save duration
     DURATION=$((ENDTIME-STARTTIME))
@@ -21,48 +28,64 @@ rec_pipeline() {
     docker exec -it arcion-demo-test-workloads-1 bash -c "clear"
     readarray -d' ' -t LOG_ID < <(docker exec -it arcion-demo-test-workloads-1 bash -c ". /tmp/ini_menu.sh; echo -n \$(basename \$CFG_DIR) \$LOG_ID")
     # save the record
-    echo ${REPL_TYPE},${SOURCE},${DESTINATION},$DURATION,$RECFILENAME,${LOG_ID[0]},${LOG_ID[1]} | tee -a $REC_DIR/clivideo.csv
+    echo ${REPL_TYPE},${SOURCE},${TARGET},$DURATION,$RECFILENAME,${LOG_ID[0]},${LOG_ID[1]} | tee -a $REC_DIR/clivideo.csv
+}
+
+start_db() {
+    local -n start_db_active_db=$1
+    local db=$2
+
+    echo "$db starting" 
+    start_db_active_db[$db]=$(( start_db_active_db[$db] + 1 ))
+    docker_compose_db up "$db"
+}
+
+stop_db() {
+    local -n stop_db_active_db=$1
+    local db=$2
+
+    stop_db_active_db[$db]=$(( stop_db_active_db[$db] - 1 ))
+    if (( ${stop_db_active_db[$db]} <= 0 )); then 
+        echo "$db pausing" 
+        docker_compose_db pause "$db"
+    else
+        echo "$db leaving up" 
+    fi
 }
 
 export REC_DIR=~/github/arcion/demokit.gtihub-io/docs/resources/asciinema
 export REPL_TYPES=(snapshot)
-export SOURCES=(s2) # db2 informix mysql oraee pg s2 sqlserver sqledge)
-export DESTINATIONS=(ase
-cockroach
-db2
-informix
-kafka
-mariadb
-minio
-mysql
-null
-oraee
-pg
-redis
-s2
-snowflake
-sqledge
-sqlserver
-yugabyte)
-
-export REPL_TYPE
-export SOURCE
-export DESTINATION
-export RECFILE
+export SOURCES=(mariadb) # db2 informix mysql oraee pg s2 sqlserver sqledge)
+export TARGETS=(informix) #cockroach db2 informix kafka mariadb minio mysql null oraee pg redis s2 sqledge sqlserver yugabytesql)
 
 mkdir -p $REC_DIR
 
-DEST_LEN=${#DESTINATIONS[@]}
-# start source 
-# start destination
+declare -A ACTIVE_DB=()
+
+docker_compose_db up arcion-demo-test
+
 for REPL_TYPE in "${REPL_TYPES[@]}"; do
-    for SOURCE in "${SOURCES[@]}"; do
-    start_db $SOURCE
-    for D_I in $(seq 0 $(($DEST_LEN - 1)) ); do
-        # rec_pipeline
-        DESTINATION=${DESTINATIONS[$D_I]}
-        echo ${REPL_TYPE},${SOURCE},${DESTINATION}
+
+    for src in "${SOURCES[@]}"; do
+
+        start_db ACTIVE_DB "$src" 
+
+        for tgt in "${TARGETS[@]}"; do
+
+            start_db ACTIVE_DB "$tgt" 
+            echo "${REPL_TYPE},${src},${tgt}"
+            docker compose ls | grep running
+
+            # DEBUG: 
+            sleep 1
+            rec_pipeline "$REPL_TYPE" "$src" "$tgt"
+
+            #wait # wait for previous start_db to complete
+            stop_db ACTIVE_DB "$tgt" 
+        done
+
+        stop_db ACTIVE_DB "$src" 
     done
-    stop_db $SOURCE
 done
-done
+
+declare -p ACTIVE_DB
--- bin/startdb.sh
+++ bin/startdb.sh
@@ -1,33 +1,17 @@
 #!/usr/bin/env bash
 
-export STARTDB_DIR=$(pwd)/$(dirname ${BASH_SOURCE[0]})
-DBS=(arcion-demo-test
-    ase
-    cockroach
-    db2
-    informix
-    kafka
-    mariadb
-    minio
-    mysql
-    pg
-    redis
-    s2
-    sqledge
-    sqlserver
-    oraee
-    oraxe
-    yugabyte
-    )
+export STARTDB_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 # return single line of docker ps
 # mysql paused 1 /abc/def/xyz
 run_docker_compose_ls() {
     local d=${d:-$1}
-    docker compose ls --filter "name=$d" \
+    local filter
+    if [ -n "${d}" ]; then filter="--filter name=$d"; fi
+    docker compose ls $filter \
         | tail -n +2 \
         | awk -F'[[:space:]]+' '{print $1 "," $2 "," $3}' \
-        | awk -F'[(),]' '{printf "%s,%s,%s,%s",$1,$2,$3,$5}'
+        | awk -F'[(),]' '{printf "%s%s,%s,%s,%s",NEWLINE,$1,$2,$3,$5;NEWLINE="\n"}'
 }
 
 # have up handle paused vs stopped
@@ -53,18 +37,20 @@ docker_compose_others() {
     local cmd=${1:-start}
     local d=${d:-${2}}
 
-    pushd ../$d || return 1
+    if [ ! -d ../$d ] || [ ! -f ../$d/docker-compose.yaml ];then return 1; fi
+
+    pushd ../$d >/dev/null || return 1
     run_docker_compose $1 $2
-    popd    
+    popd >/dev/null   
 }
 
 docker_compose_ora() {
     local cmd=${1}
     local d=${d:-${2}}
 
-    pushd ../oracle/$d || return 1
+    pushd ../oracle/$d >/dev/null || return 1
     run_docker_compose $1 $2
-    popd
+    popd >/dev/null
 }
 
 # yb does not come back up 
@@ -72,10 +58,10 @@ docker_compose_yb() {
     local cmd=${1}
     local d=${d:-${2}}
 
-    pushd ../$d || return 1
+    pushd ../$d >/dev/null || return 1
     case ${cmd} in up|unpause|restart|start) docker compose down -v;; esac   
     run_docker_compose $1 $2
-    popd
+    popd >/dev/null
 }
 
 # run docker compose cmd database
@@ -83,15 +69,15 @@ docker_compose_db() {
     local cmd=${1}
     local d=${2}
 
-    if [ -z "${d}" ]; then 
-        echo "specify database" && return 1
+    if [ -z "${d}" ] || [ -z "${cmd}" ]; then 
+        echo "specify \$1=cmd (up|pause) and \$2=database" >&2 && return 1
     fi
 
-    pushd $STARTDB_DIR || return 1
+    pushd $STARTDB_DIR >/dev/null || return 1
     case ${d} in 
-        yugabyte) docker_compose_yb $1 $2;;
-        oraxe|oraee) docker_compose_ora $1 $2;;
-        *) docker_compose_others $1 $2;;
+        kafka|yugabyte) docker_compose_yb "$1" "$2";;
+        oraxe|oraee) docker_compose_ora "$1" "$2";;
+        *) docker_compose_others "$1" "$2";;
     esac
-    popd 
+    popd >/dev/null
 }
