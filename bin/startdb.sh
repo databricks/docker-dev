331a754b10e67eb4a85932764ea89d8bb3d8e96e
Robert S Lee <sangkyulee@gmail.com>
2023-09-01 14:33:17 -0400

    gdrive download

--- bin/startdb.sh
+++ bin/startdb.sh
@@ -10,6 +10,13 @@ set_machine() {
     export MACHINE=$(uname -p)    
 }
 
+downloadFromGdrive() {
+    local FILEID=$1
+    local FILENAME=$2
+
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O ${FILENAME} && rm -rf /tmp/cookies.txt
+}
+
 install_oraee() {
     local found=$(docker images -q "oracle/database:19.3.0-ee")
     if [[ -z "${found}" ]]; then 
