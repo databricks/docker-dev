7d4487c955d2e74500073a1d85eb4a9fab12daac
Robert S Lee <sangkyulee@gmail.com>
2023-09-23 14:39:05 -0400

    add pg16

--- install.sh
+++ install.sh
@@ -133,18 +133,23 @@ choose_start_cli() {
     fi
 }
 
+getDockerComposeFiles() {
+        find * -maxdepth 2 \
+        -not \( -path "${DOCKERDEV_NAME:-.}" -prune \) \
+        -name "*compose*.yaml" | getNameVerFromYaml
+}
+
 # ARCION_DOCKER_DBS="mysql,ON mysql,OFF"
 chooseDataProviders() {
 
     composefile_output=/tmp/composefile.$$.txt      # Name-ver
     composels_output=/tmp/composels.$$.txt          # Name-ver,Status(es),RunningCount,NotRunningCount,ConfigFile    
     whiptail_input=/tmp/whiptail_input.$$.txt       # Name-ver,Status(es),ON|OFF
-    whiptail_output=/tmp/whiptailselection.$$.txt   # Name-ver,ON|OFF
+    whiptail_unsorted=/tmp/whiptailselection.$$.txt # Name-ver,ON|OFF
+    whiptail_output=/tmp/whiptailselectionsorted.$$.txt   # Name-ver,ON|OFF
 
     # all docker compose dirs. for oracle/oraxe show oraxe
-    find * -maxdepth 2 \
-        -not \( -path "${DOCKERDEV_NAME}" -prune \) \
-        -name "*compose*.yaml" | getNameVerFromYaml | sort -u > ${composefile_output}
+    getDockerComposeFiles | sort -t, -u > ${composefile_output}
 
     # all running and stopped docker compose 
     # Name-ver,Status(es),RunningCount,NotRunningCount,ConfigFile
@@ -154,14 +159,14 @@ chooseDataProviders() {
     # left join (-a 1)
     # fields 1.1 file 1.field 1 (-o)
     # name status(1/2)_dir 
-    join -t, -a 1 -o "1.1 2.2 2.3 2.4 2.5"  ${composefile_output} ${composels_output} | \
+    join -t, -a 1 -o "1.2 2.3 2.4 2.5 2.6"  ${composefile_output} ${composels_output} | \
         awk -F',' '{
             # $4==0 make sure nothing is in exited / paused / stopped status
             if (index($2,"running") && $4==0) {onoff="ON"} 
             else {onoff="OFF"}; 
             if ($3=="" && $4=="") printf "%s,,%s\n",$1,onoff;
-            else printf "%s,running(%s)/not(%s),%s\n",$1,$3,$4,onoff;}' \
-        > ${whiptail_input}   
+            else printf "%s,running(%s)/not(%s),%s\n",$1,$3,$4,onoff;}' | \
+        sort > ${whiptail_input}   
     readarray -d ',' -t whiptailmenu < <(cat ${whiptail_input} | tr '\n' ',')
 
     $CLIMENU --title "Select / Deselet To Start/Stop Service" --output-fd 4 --separate-output \
@@ -170,7 +175,9 @@ chooseDataProviders() {
     cd mysql; docker compose stop" \
     0 0 0 \
     "${whiptailmenu[@]}" \
-    4> ${whiptail_output}
+    4> ${whiptail_unsorted}
+
+    sort ${whiptail_unsorted} > ${whiptail_output}
 
     if [[ "$?" != "0" ]]; then return; fi
 
@@ -296,13 +303,28 @@ install_ora() {
     esac 
 }
 
-getNameVerFromYaml() {
+# make oraxe/docker-compose-v11.yaml to oraxe-v11
+getNameVerFromYamlRemoveSlash() {
     local F=${1:-1}
     awk -F'\t' -v F=${F} '{split($F,c,"/"); n[$1]=c[length(c)-1]; split(c[length(c)],y,"[-|.]"); x=y[length(y)-1]; if (x=="compose") v[$F]=""; else v[$1]=("-" x);} 
         END {for (k in n) {printf "%s%s\n",n[k],v[k]}}'
 }
 
-# Name,Status(es),RunningCount,NotRunningCount,ConfigFile
+getNameVerFromYaml() {
+    local F=${1:-1}
+    # dir dirname
+    # dcf docker compose yaml file name
+    awk -F'/' -v F=${F} '{
+        dir=$1; 
+        for (i=2;i<NF;i++) {dir=(dir "/" $i)}; 
+        split($NF,dcy,"[-|.]"); v=dcy[length(dcy)-1]; 
+        if (v=="compose") v=""; else v=("-" v); 
+        printf "%s,%s%s\n",$0,dir,v;
+        }' 
+}
+
+
+# ConfigFile,Name[-ver],Status(es),RunningCount,NotRunningCount
 run_docker_compose_ls() {
     local d=${1}
     local filter
@@ -316,8 +338,9 @@ run_docker_compose_ls() {
     docker inspect ${containerid[@]} | \
     jq -r '.[] | [.Config.Labels."com.docker.compose.project.config_files", .State.Status] | @tsv' | \
     awk -F'\t' '{split($1,c,"/"); n[$1]=c[length(c)-1]; split(c[length(c)],y,"[-|.]"); x=y[length(y)-1]; if (x=="compose") v[$1]=""; else v[$1]=("-" x);}
-    {r[$1]+=0; nr[$1]+=0; s[$1]=(s[$1] $2); if ($2=="running") r[$1]++; else nr[$1]++; } 
-    END {for (k in s) {printf "%s%s,%s,%s,%s,%s\n",n[k],v[k],s[k],r[k],nr[k],k}}'
+    {r[$1]+=0; nr[$1]+=0; s[$1]=(s[$1] $2 "|"); if ($2=="running") r[$1]++; else nr[$1]++; } 
+    END {for (k in s) {printf "%s,%s%s,%s,%s,%s\n",k,n[k],v[k],s[k],r[k],nr[k]}}' | \
+    sed "s|^${DOCKERDEV_BASEDIR}/||" 
     # per line vars
     # c= config split by / array
     # y= yaml split by [-|.]
@@ -337,16 +360,16 @@ run_docker_compose() {
     local d=${2}
     local compose_file=${3:-"docker-compose.yaml"}
     
-    # Name[-ver],Status(es),RunningCount,NotRunningCount,ConfigFile
+    # ConfigFile,Name[-ver],Status(es),RunningCount,NotRunningCount
     readarray -d',' -t DOCKER_LS < <(run_docker_compose_ls "$d" | grep "${compose_file}")
     case ${cmd} in 
-        up) if [[ "${DOCKER_LS[1]}" =~ "paused" ]]; then 
+        up) if [[ "${DOCKER_LS[2]}" =~ "paused" ]]; then 
                 $ARCION_DOCKER_COMPOSE -f ${compose_file} unpause || abort "${pwd} $ARCION_DOCKER_COMPOSE unpause failed" 
-            elif [[ "${DOCKER_LS[1]}" =~ "exited" ]]; then 
+            elif [[ "${DOCKER_LS[2]}" =~ "exited" ]]; then 
                 $ARCION_DOCKER_COMPOSE -f ${compose_file} start || abort "${pwd} $ARCION_DOCKER_COMPOSE start failed"
             else 
                 export DOCKER_BUILDKIT=0
-                # $ARCION_DOCKER_COMPOSE build
+                $ARCION_DOCKER_COMPOSE build
                 $ARCION_DOCKER_COMPOSE -f ${compose_file} up -d || abort "${pwd} $ARCION_DOCKER_COMPOSE up -d failed" 
             fi
             ;;
@@ -374,6 +397,7 @@ docker_compose_others() {
     fi
 
     pushd $d >/dev/null || return 1
+    d=${d##*/}  # ##=greedy trim, *=match anything, /=until the lasts / returning the basename
     run_docker_compose "$cmd" "$d" "${compose_file}"
     if [ -n "$WAIT_TO_COMPLETE" ]; then 
         $WAIT_TO_COMPLETE
@@ -426,7 +450,7 @@ wait_arcion_demo() {
 docker_compose_db() {
     local cmd=${1}
     local d=${2}
-    local compose_file=${3}
+    local ver=${3}
     local WAIT_TO_COMPLETE=${4}
 
     echo $cmd $d $ver>&2
@@ -437,14 +461,13 @@ docker_compose_db() {
         echo "specify \$1=cmd (up|down|start|stop|restart) and \$2=database" >&2 && return 1
     fi
 
-    compose_file=docker-compose.yaml
-    if [[ -n "$ver" ]]; then compose_file="${compose_file}-${ver}.yaml"; fi
+    if [[ -z "$ver" ]]; then compose_file="docker-compose.yaml"; else compose_file="docker-compose-${ver}.yaml"; fi
 
     pushd $DOCKERDEV_BASEDIR >/dev/null || return 1
     case ${d} in 
-        arcdemo|arctest) docker_compose_others "$1" "$2" "$compose_file" "wait_arcion_demo";;
+        arcdemo|arcdemo/arctest) docker_compose_others "$1" "$2" "$compose_file" "wait_arcion_demo";;
         kafka|redis|yugabyte) docker_compose_yb "$1" "$2" "$compose_file";;
-        orafree|oraxe|oraee) docker_compose_ora "$1" "$2" "$compose_file";;
+        oracle/orafree|oracle/oraxe|oracle/oraee) docker_compose_ora "$1" "$2" "$compose_file";;
         *) docker_compose_others "$1" "$2" "$compose_file";;
     esac
     popd >/dev/null
@@ -520,12 +543,12 @@ setWhiptailDialog() {
     # git exists
     [ -n "${CLIMENU}" ] && return 0
 
-    if [[ -n $(which dialog) ]]; then 
-        echo "dialog founded." 
-        export CLIMENU="dialog --no-collapse"
-    elif [[ -n $(which whiptail) ]]; then 
+    if [[ -n $(which whiptail) ]]; then 
         echo "whiptail founded." 
         export CLIMENU=whiptail
+    elif [[ -n $(which dialog) ]]; then 
+        echo "dialog founded." 
+        export CLIMENU="dialog --no-collapse"
     else     
 abort "whiptail or dialog not found.
 on OSX 
@@ -630,7 +653,9 @@ startDatabases() {
     echo ${ARCION_DOCKER_DBS[@]}
     for db in ${ARCION_DOCKER_DBS[@]}; do
         readarray -d',' -t name_ver_onoff < <(echo "${db}" | awk -F'[-|,]' 'NF==2 {printf "%s,,%s",$1,$2} NF==3 {printf "%s,%s,%s",$1,$2,$3}')
+
         declare -p name_ver_onoff
+
         case "${name_ver_onoff[2]}" in
         "ON") docker_compose_db "up" "${name_ver_onoff[0]}" "${name_ver_onoff[1]}";;
         "OFF") docker_compose_db "stop" "${name_ver_onoff[0]}" "${name_ver_onoff[1]}";;
