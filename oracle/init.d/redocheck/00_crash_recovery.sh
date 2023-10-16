f59df961298559f9f6664a46f14cf1be9ab29e85
Robert S Lee <sangkyulee@gmail.com>
2023-10-16 15:48:24 -0400

    mount tmpfs with exec

--- oracle/init.d/redocheck/00_crash_recovery.sh
+++ oracle/init.d/redocheck/00_crash_recovery.sh
@@ -11,17 +11,22 @@ EOF
 
     scn=$(cat $LOGDIR/backup.log | grep -m 1 -A 1 CURRENT_SCN | tail -1)
 
-    rman target / <<EOF
-    startup mount;
-    run
-    {
-    set until sequence $scn;
-    restore database;
-    recover database;
-    }
-    alter database open resetlogs;
-    shutdown immediate;
+    if [ -z "$scn" ]; then
+        rm -rf /opt/oracle/oradata/*
+        rm -rf /opt/oracle/oradata/.*
+    else
+        rman target / <<EOF
+        startup mount;
+        run
+        {
+        set until sequence $scn;
+        restore database;
+        recover database;
+        alter database open resetlogs;
+        }
+        shutdown immediate;
 EOF
+    fi
 
     set +x
 }
